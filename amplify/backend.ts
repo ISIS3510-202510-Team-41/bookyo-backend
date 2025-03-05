f60c9c7652554944ece2f75338530fac8ce5d11a
darkoyd <n.londonoc@uniandes.edu.co>
2025-03-04 23:18:16 -0500

    Initial commit

---
+++ amplify/backend.ts
@@ -0,0 +1,100 @@
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { storage } from './storage/resource';
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { CfnApp } from "aws-cdk-lib/aws-pinpoint";
+import { Stack } from "aws-cdk-lib/core";
+import { CfnMap } from 'aws-cdk-lib/aws-location';
+
+const backend = defineBackend({
+  auth,
+  data,
+  storage
+});
+
+
+const analyticsStack = backend.createStack("analytics-stack");
+
+// create a Pinpoint app
+const pinpoint = new CfnApp(analyticsStack, "Pinpoint", {
+  name: "bookyo-pinpont",
+});
+
+// create an IAM policy to allow interacting with Pinpoint
+const pinpointPolicy = new Policy(analyticsStack, "PinpointPolicy", {
+  policyName: "PinpointPolicy",
+  statements: [
+    new PolicyStatement({
+      actions: ["mobiletargeting:UpdateEndpoint", "mobiletargeting:PutEvents"],
+      resources: [pinpoint.attrArn + "/*"],
+    }),
+  ],
+});
+
+// apply the policy to the authenticated and unauthenticated roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(pinpointPolicy);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(pinpointPolicy);
+
+// patch the custom Pinpoint resource to the expected output configuration
+backend.addOutput({
+  analytics: {
+    amazon_pinpoint: {
+      app_id: pinpoint.ref,
+      aws_region: Stack.of(pinpoint).region,
+    }
+  },
+});
+
+
+const geoStack = backend.createStack("geo-stack");
+
+// create a location services map
+const map = new CfnMap(geoStack, "Map", {
+  mapName: "bookyo-map",
+  description: "Map",
+  configuration: {
+    style: "VectorEsriNavigation",
+  },
+  pricingPlan: "RequestBasedUsage",
+  tags: [
+    {
+      key: "name",
+      value: "myMap",
+    },
+  ],
+});
+
+// create an IAM policy to allow interacting with geo resource
+const myGeoPolicy = new Policy(geoStack, "GeoPolicy", {
+  policyName: "bookyo-geo-policy",
+  statements: [
+    new PolicyStatement({
+      actions: [
+        "geo:GetMapTile",
+        "geo:GetMapSprites",
+        "geo:GetMapGlyphs",
+        "geo:GetMapStyleDescriptor",
+      ],
+      resources: [map.attrArn],
+    }),
+  ],
+});
+
+// apply the policy to the authenticated and unauthenticated roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(myGeoPolicy);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(myGeoPolicy);
+
+backend.addOutput({
+  geo: {
+    aws_region: geoStack.region,
+    maps: {
+      items: {
+        [map.mapName]: {
+          style: "VectorEsriNavigation",
+        },
+      },
+      default: map.mapName,
+    },
+  },
+});
---
+++ amplify/data/resource.ts
@@ -0,0 +1,178 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Wishlist: a.model({
+    id: a.id().required(),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    books: a.hasMany("BookWishlist", "wishlistId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  BookWishlist: a.model({
+    bookId: a.id(),
+    wishlistId: a.id().required(),
+    book: a.belongsTo("Book", "bookId"),
+    list: a.belongsTo("Wishlist", "wishlistId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  Book: a.model({
+    id: a.id().required(),
+    title: a.string().required(),
+    isbn: a.string().required(),
+    thumbnail: a.string(),
+    authorId: a.string().required(),
+    author: a.belongsTo("Author", "authorId"),
+    categories: a.hasMany("BookCategory", "bookId"),
+    ratings: a.hasMany("BookRating", "bookId"),
+    listings: a.hasMany("Listing", "bookId"),
+    wishlists: a.hasMany("BookWishlist", "bookId"),
+    libraries: a.hasMany("BookLibrary", "bookId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]).secondaryIndexes((index) => [index("isbn")]),
+
+  UserLibrary: a.model({
+    id: a.id().required(),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    books: a.hasMany("BookLibrary", "libraryId"),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  BookLibrary: a.model({
+    bookId: a.id(),
+    libraryId: a.id().required(),
+    book: a.belongsTo("Book", "bookId"),
+    library: a.belongsTo("UserLibrary", "libraryId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  BookCategory: a.model({
+    categoryId: a.string(),
+    bookId: a.string().required(),
+    category: a.belongsTo("Category", "categoryId"),
+    book: a.belongsTo("Book", "bookId"),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+  Author: a.model({
+    id: a.id().required(),
+    name: a.string().required(),
+    books: a.hasMany("Book", "authorId"),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  User: a.model({
+    email: a.string().required(),
+    firstName: a.string(),
+    lastName: a.string(),
+    address: a.string(),
+    phone: a.string(),
+    library: a.hasOne("UserLibrary", "userId"),
+    ratingsReceived: a.hasMany("UserRating", "ratedId"),
+    ratings: a.hasMany("UserRating", "userId"),
+    listings: a.hasMany("Listing", "userId"),
+    wishlist: a.hasOne("Wishlist", "userId"),
+    cart: a.hasOne("Cart", "userId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]).identifier(["email"]),
+
+  UserRating: a.model({
+    id: a.id().required(),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    ratedId: a.string().required(),
+    ratedUser: a.belongsTo("User", "ratedId"),
+    rating: a.integer().required(),
+    description: a.string(),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  Notifications: a.model({
+    id: a.id().required(),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    message: a.string().required(),
+    read: a.boolean().required(),
+
+  }).authorization(allow => [
+    allow.owner().to(['update'])
+  ]),
+
+  Category: a.model({
+    id: a.id().required(),
+    name: a.string().required(),
+    books: a.hasMany("BookCategory", "categoryId"),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  BookRating: a.model({
+    id: a.id().required(),
+    bookId: a.string().required(),
+    book: a.belongsTo("Book", "bookId"),
+    rating: a.integer().required(),
+    description: a.string(),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+
+  Listing: a.model({
+    id: a.id().required(),
+    bookId: a.string().required(),
+    book: a.belongsTo("Book", "bookId"),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    price: a.float().required(),
+    status: a.enum(["available", "unavailable", "sold"]),
+    photos: a.string().array().required(),
+    cartId: a.id(),
+    cart: a.belongsTo("Cart", "cartId")
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+  Cart: a.model({
+    id: a.id().required(),
+    userId: a.string().required(),
+    user: a.belongsTo("User", "userId"),
+    listings: a.hasMany("Listing", "cartId"),
+    state: a.enum(["active", "completed", "shipping"]),
+  }).authorization(allow => [
+    allow.authenticated().to(['read']),
+    allow.owner().to(['create', 'update', 'delete'])
+  ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+    // API Key is used for public access
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
